a27d45c87bd83b765ee86558d01c73e8cfa291ab
Alexsander Vyshnyvetskyy <wdmg.com.ua@gmail.com>
2023-06-21 12:11:29 +0300

    Remove version info from component

--- components/Mails.php
+++ components/Mails.php
@@ -7,10 +7,9 @@ namespace wdmg\mailer\components;
  * Yii2 MailerHelper
  *
  * @category        Component
- * @version         1.3.6
  * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
  * @link            https://github.com/wdmg/yii2-mailer
- * @copyright       Copyright (c) 2019 - 2021 W.D.M.Group, Ukraine
+ * @copyright       Copyright (c) 2019 - 2023 W.D.M.Group, Ukraine
  * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
  *
  */
